ddaee92f5e3ce32405f210f8d3edf18ff9357229
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-24 18:15:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)   
+    index.between?(0,8) && !position_taken?(board, index)
 end
 def input_to_index(user_input)
    user_input.to_i - 1
